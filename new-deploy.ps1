095674afbb46af9c2d4da8b3ee2e242b1f909114
Andrii_Kopytsia <andrey.kopitsa@gmail.com>
2017-11-21 16:53:21 +0200

    change

--- new-deploy.ps1
+++ new-deploy.ps1
@@ -1,6 +1,8 @@
 $MySubscribtion = "3c1acdbb-6c4a-4709-b589-1e6fe59ccd99"
 $MyLocation = "West Europe"
 $MyResourceGroup = "TaskOne"
+$MyResourceGroupDeployment = "TaskOneDeployment"
+$MyTemplationFile = "C:\Users\Andrii_Kopytsia\Documents\azure-scripts\task-1\template.json"
 
 Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionName $MySubscribtion
@@ -14,3 +16,4 @@ if ($notPresent) {
 }
 
 
+New-AzureRmResourceGroupDeployment -Name $MyResourceGroupDeployment -ResourceGroupName $MyResourceGroup  -TemplateFile $MyTemplationFile -storageAccountType Standard_LRS
